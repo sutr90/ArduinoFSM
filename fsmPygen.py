12bcee7fc4036e27037cd46eabcb97884b5f2680
sutr90 <sutr90@gmail.com>
2015-08-17 07:40:37 +0200

    Initial commit.

---
+++ fsmPygen.py
@@ -0,0 +1,96 @@
+def create_events(events):
+    evt_str = "typedef enum {"
+    for e in events:
+        evt_str += "{}, ".format(e)
+
+    evt_str += "} Event;\n"
+    evt_str += "Event currentEvent;\n"
+    return evt_str
+
+
+def create_states(states):
+    state_str = "typedef enum {"
+    for s in states:
+        state_str += "{}, ".format(s)
+
+    state_str += "} State;\n"
+    state_str += "State currentState;\n"
+    return state_str
+
+
+def create_fsm_table(table):
+    states = sorted(table.keys())
+    events = sorted(table.values()[0].keys())
+
+    col_width = max([len(max(row.values(), key=len)) for row in table.values()])
+
+    s_count = len(states)
+    e_count = len(events)
+
+    fsm_str = "int fsmTable[{0}][{1}] = {{\n".format(s_count, e_count)
+
+    fsm_str += "\t/*"
+
+    for e in events:
+        fsm_str += "{:>{}}, ".format(e, col_width)
+
+    fsm_str += "*/\n"
+    for s in states:
+        fsm_str += "\t{ "
+        for e in events:
+            fsm_str += "{:>{}}, ".format(table[s][e], col_width)
+        fsm_str += "}},/* {} */\n".format(s)
+
+    fsm_str += "};\n"
+
+    return fsm_str
+
+
+def create_poll():
+    poll_str = "void pollEvents() {\n"
+    poll_str += "\t/* TODO: update currentEvent */\n"
+    poll_str += "}\n"
+    return poll_str
+
+
+def create_actions(states):
+    actions_str = ""
+    for s in states:
+        actions_str += "void action{}(){{\n".format(s)
+        actions_str += "\t/* TODO: add action for state {} */\n".format(s)
+        actions_str += "}\n"
+
+    return actions_str
+
+
+def create_eval_state(states, prefix):
+    eval_str = "void evalState(){\n"
+    eval_str += "\tswitch(currentState){\n"
+
+    for s in states:
+        eval_str += "\t\tcase {}:\n".format(prefix + s)
+        eval_str += "\t\t\taction{}();\n".format(s)
+        eval_str += "\t\t\tbreak;\n"
+
+    eval_str += "\t}\n"
+    eval_str += "}\n"
+
+    return eval_str
+
+
+def create_setup(start):
+    setup_str = "void setup(){\n"
+    setup_str += "\tcurrentState = {};\n".format(start)
+    setup_str += "}\n"
+
+    return setup_str
+
+
+def create_loop():
+    loop_str = "void loop() {\n"
+    loop_str += "\tpollEvents();\n"
+    loop_str += "\tcurrentState = (State) fsmTable[currentState][currentEvent];\n"
+    loop_str += "\tevalState();\n"
+    loop_str += "}\n"
+
+    return loop_str
---
+++ main.py
@@ -0,0 +1,88 @@
+import collections
+import pydot_ng as pydot
+from fsmPygen import *
+
+graph = pydot.graph_from_dot_file('C:\Users\VF\Desktop\graph1.gv')
+edges = graph.get_edges()
+
+# state prefix
+sp = 'st_'
+# event prefix
+ep = 'evt_'
+# null event
+enull = ep + 'null'
+
+
+def extract_state_names(edges_p):
+    state_set = set()
+
+    for e in edges_p:
+        state_set.add(sp + e.get_source())
+        state_set.add(sp + e.get_destination())
+
+    return sorted(state_set)
+
+
+def extract_event_names(edges_p):
+    event_set = set()
+
+    for e in edges_p:
+        event_set.add(ep + e.get_label())
+
+    event_set.add(enull)
+
+    return sorted(event_set)
+
+
+def generate_state_table(edges_p):
+    events = extract_event_names(edges_p)
+    states = extract_state_names(edges_p)
+
+    d = collections.defaultdict(dict)
+
+    for s in states:
+        for e in events:
+            d[s][e] = s
+
+    for e in edges_p:
+        d[sp + e.get_source()][ep + e.get_label()] = sp + e.get_destination()
+
+    return d
+
+
+def pretty(d, indent=0):
+    for key, value in d.iteritems():
+        print '\t' * indent + str(key) + '\t' * (indent + 1) + str(value)
+
+
+def create_file(states, events, table):
+    cfile = create_events(events) + "\n"
+    cfile += create_states(states) + "\n"
+    cfile += create_fsm_table(table) + "\n"
+    cfile += create_poll() + "\n"
+
+    actions = [s[len(sp):] for s in states]
+
+    cfile += create_actions(actions) + "\n"
+    cfile += create_eval_state(actions, sp) + "\n"
+    cfile += create_setup(states[0]) + "\n"
+    cfile += create_loop() + "\n"
+
+    return cfile
+
+
+def main():
+    s = extract_state_names(edges)
+    # print(s)
+
+    e = extract_event_names(edges)
+    # print(e)
+
+    t = generate_state_table(edges)
+    # pretty(t)
+
+    print create_file(s, e, t)
+
+
+if __name__ == '__main__':
+    main()
